3198de3f24cc1891cfd4a61719ed6bd1798c3985
JJ <jeonghojin03@gmail.com>
2023-01-03 02:05:25 +0900

    feat: iOS 초기 설정

---
+++ ios/ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by 정호진 on 2023/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
