a451fbd1885a052a78c776dfa03400ea083ca335
JJ <jeonghojin03@gmail.com>
2023-01-20 00:39:33 +0900

    chore: MVVM패턴 적용 및 Storyboard 제거

---
+++ ios/ios/Delegate/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  ios
+//
+//  Created by 정호진 on 2023/01/03.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+     
+    }
+
+
+}
+
