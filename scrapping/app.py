d74fca14a172f5fa4207a37c3239bf682ea41a35
ohksj77 <ohksj77@gmail.com>
2023-01-26 00:32:35 +0900

    feat: add nginx gunicorn to scrapping

--- scrapping/app.py
+++ scrapping/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request
 from flask_restx import Resource, Api
 from flask_cors import CORS
 from bs4 import BeautifulSoup
-from models import Search, Result
 import requests
 
 app = Flask(__name__)
@@ -12,36 +11,32 @@ api = Api(app)
 
 ns = api.namespace('/', description='GitRank API')
 
-@ns.route('/api/search/git-repos', methods=['GET'])
+@ns.route('/scrap', methods=['GET'])
 class scrapping(Resource):
     
     '''레포명으로 검색한 페이지를 스크래핑한다'''
     def get(self):
         results = []
+        mapper = {'users' : 'user_search_results', 'repositories' : 'repo-list'}
         
         repository = request.args.get('name')
-        page = int(request.args.get('page'))
+        search_type = request.args.get('type')
+        page = request.args.get('page')
         
-        s = Search(search_word=repository, search_type='repository', search_page=page)
-        
-        result = requests.get('https://github.com/search?q=' + repository + '&type=Repositories&p=' + str(page))
+        result = requests.get('https://github.com/search?q=' + repository + '&type=' + search_type + '&p=' + page)
         result.raise_for_status()
         soup = BeautifulSoup(result.text, "lxml")
-        repo_list = soup.find('ul', attrs={"class" : "repo-list"})
+        repo_list = soup.find('ul', attrs={"class" : mapper[search_type]})
         em_tag_list = repo_list.find_all('a', attrs={"class" : "v-align-middle"})
             
         for em_tag in em_tag_list:
-            repository_name = em_tag.text
-            
-            r = Result(result=repository_name)
-            results.append(r)
+            results.append(em_tag.text)
 
         response = {}
-        response['search'] = s.serialize()
-        response['result'] = [result.serialize() for result in results]
+        response['result'] = [{'name' : result} for result in results]
         
-        return ((response), 200)
-
+        return (response, 200)
+            
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True, host='0.0.0.0')
+    app.run(port=5000, debug=True)
