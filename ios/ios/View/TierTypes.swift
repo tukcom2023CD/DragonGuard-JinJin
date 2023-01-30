90f5c4208af19c6c03fab94ebd533bce78e6f300
JJ <jeonghojin03@gmail.com>
2023-01-30 16:42:39 +0900

    feat: setting tableview 화면이동 설정

---
+++ ios/ios/View/TierTypes.swift
@@ -0,0 +1,17 @@
+//
+//  TierTypes.swift
+//  ios
+//
+//  Created by 정호진 on 2023/01/30.
+//
+
+import Foundation
+import UIKit
+
+final class TierTypes: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+        self.navigationItem.title = "Tier 종류"
+    }
+}
