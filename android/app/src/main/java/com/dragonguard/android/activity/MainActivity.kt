c5222ac12b7188422f38d82c9f334862f037f698
posite <su84053@gmail.com>
2023-01-30 22:01:04 +0900

    feat: 메인화면 2차 구성

--- android/app/src/main/java/com/dragonguard/android/activity/MainActivity.kt
+++ android/app/src/main/java/com/dragonguard/android/activity/MainActivity.kt
@@ -2,24 +2,31 @@ package com.dragonguard.android.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.dragonguard.android.R
 import com.dragonguard.android.databinding.ActivityMainBinding
+import com.dragonguard.android.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-//    var count = 0
+    var viewmodel = MainViewModel()
+
+    //    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.mainActivity = this
+        binding.mainViewModel = viewmodel
 
 
         //로그인 화면으로 넘어가기
         val intent = Intent(applicationContext, LoginActivity::class.java)
         startActivity(intent)
 
+        setSupportActionBar(binding.toolbar) //커스텀한 toolbar를 액션바로 사용
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 /*        화면전환 테스트용 버튼
         binding.btnadd.setOnClickListener {
             count++
@@ -27,12 +34,26 @@ class MainActivity : AppCompatActivity() {
         }
 
  */
+        viewmodel
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu1, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     /*
-    검색하는 화면으로 넘어가는 버튼 listener 구현
-     */
-    fun searchRepo(){
+        검색하는 화면으로 넘어가는 버튼 listener 구현
+         */
+    fun searchRepo() {
         val intent = Intent(applicationContext, SearchActivity::class.java)
         startActivity(intent)
     }
--- android/app/src/main/java/com/dragonguard/android/viewmodel/MainViewModel.kt
+++ android/app/src/main/java/com/dragonguard/android/viewmodel/MainViewModel.kt
@@ -1,4 +1,30 @@
 package com.dragonguard.android.viewmodel
 
+
+import androidx.lifecycle.MutableLiveData
+
+
 class MainViewModel {
+    var onSearchListener = MutableLiveData<String>()
+    var onIconClickListener = MutableLiveData<Boolean>()
+
+    fun clickSearchIcon() {
+        onIconClickListener.value = true
+    }
+
+    fun searchRepo() {
+
+    }
+
+    fun lookRanking() {
+
+    }
+
+    fun compareRepo() {
+
+    }
+
+    fun schoolRanking() {
+
+    }
 }
