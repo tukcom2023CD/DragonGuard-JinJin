86467dd5851d0528c9d11c3817d89d8f975b7c09
posite <su84053@gmail.com>
2023-01-31 18:25:51 +0900

    feat: 설정 배너 구성및 작동 구현, 홈메뉴 구현

--- android/app/src/main/java/com/dragonguard/android/activity/MainActivity.kt
+++ android/app/src/main/java/com/dragonguard/android/activity/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.dragonguard.android.activity
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.MotionEvent
-import android.view.View
+import android.view.*
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -16,6 +14,7 @@ import com.dragonguard.android.viewmodel.MainViewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var viewmodel = MainViewModel()
+    private lateinit var versionDialog : Dialog
 
     //    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +22,9 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.mainViewModel = viewmodel
 
-
+        versionDialog = Dialog(this)
+        versionDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        versionDialog.setContentView(R.layout.version_dialog)
         //로그인 화면으로 넘어가기
         val intent = Intent(applicationContext, LoginActivity::class.java)
         startActivity(intent)
@@ -41,6 +42,10 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun showDialog() {
+        versionDialog.show()
+    }
+
     private fun closeKeyboard() {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(binding.searchName.windowToken, 0)
@@ -52,13 +57,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu1, binding.toolbarBottom.menu)
+        menuInflater.inflate(R.menu.menu1, binding.toolbar.menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
+            R.id.logout->{
 
+            }
+            R.id.faq->{
+                val intent = Intent(applicationContext, FaqActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                startActivity(intent)
+            }
+            R.id.token_criterion->{
+                val intent = Intent(applicationContext, CriterionActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                startActivity(intent)
+            }
+            R.id.version->{
+                showDialog()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
