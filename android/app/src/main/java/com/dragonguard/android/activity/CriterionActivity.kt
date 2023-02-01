bb62100adbd0b5952a4f4149f5f7a25e58b5aa63
posite <su84053@gmail.com>
2023-02-01 17:48:14 +0900

    fix: workspace.xml 버그 수정 및 토큰부여기준 화면의 뒤로가기, 홈 아이콘 수정

--- android/app/src/main/java/com/dragonguard/android/activity/CriterionActivity.kt
+++ android/app/src/main/java/com/dragonguard/android/activity/CriterionActivity.kt
@@ -21,13 +21,20 @@ class CriterionActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar) //커스텀한 toolbar를 액션바로 사용
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_home_24)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home, binding.toolbar.menu)
+        return true
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             android.R.id.home->{
+                finish()
+            }
+            R.id.home_menu->{
                 val intent = Intent(applicationContext, MainActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
                 intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
