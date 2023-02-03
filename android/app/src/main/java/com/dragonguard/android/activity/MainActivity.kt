94a9744fa3d02782087bc42e7bf384465f204d3a
posite <su84053@gmail.com>
2023-02-03 21:56:35 +0900

    chore: 메인액티비티 주석 추가 및 뒤로가기 두번 시

--- android/app/src/main/java/com/dragonguard/android/activity/MainActivity.kt
+++ android/app/src/main/java/com/dragonguard/android/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -19,6 +20,7 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var viewmodel = MainViewModel()
+    private var backPressed : Long = 0
 
     //    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,24 +28,20 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.mainViewModel = viewmodel
 
-        searchTier(1)
+        searchUser(1)
 
         //로그인 화면으로 넘어가기
         val intent = Intent(applicationContext, LoginActivity::class.java)
         startActivity(intent)
 
-/*        화면전환 테스트용 버튼
-        binding.btnadd.setOnClickListener {
-            count++
-            binding.btnadd.setText(count.toString())
-        }
 
- */
+//        랭킹 보러가기 버튼 눌렀을 때 랭킹 화면으로 전환
         binding.lookRanking.setOnClickListener {
             val intent = Intent(applicationContext, RankingsActivity::class.java)
             startActivity(intent)
         }
 
+//        유저 아이디, 프로필을 눌렀을 때 메뉴 화면으로 전환
         viewmodel.onUserIconSelected.observe(this, Observer {
             if(viewmodel.onUserIconSelected.value == true){
                 val intent = Intent(applicationContext, MenuActivity::class.java)
@@ -51,6 +49,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+//        검색창, 검색 아이콘 눌렀을 때 검색화면으로 전환
         viewmodel.onSearchClickListener.observe(this, Observer {
             if(viewmodel.onSearchClickListener.value == true) {
                 val intent = Intent(applicationContext, SearchActivity::class.java)
@@ -60,12 +59,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun searchTier(id: Int){
+//  메인화면의 유저 정보 검색하기
+    private fun searchUser(id: Int){
         var tier = ""
         val coroutine = CoroutineScope(Dispatchers.Main)
         coroutine.launch {
             val resultDeferred = coroutine.async(Dispatchers.IO) {
-                viewmodel.getSearchTierResult(1)
+                viewmodel.getSearchTierResult(id)
             }
             tier = resultDeferred.await()
             Log.d("api 시도", "api result에 넣기 $tier")
@@ -73,14 +73,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //    edittext 키보드 제거
     private fun closeKeyboard() {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(binding.searchName.windowToken, 0)
     }
 
+//    화면을 눌렀을때 키보드 제거
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
         closeKeyboard()
         return super.dispatchTouchEvent(ev)
     }
 
+    override fun onBackPressed() {
+//        super.onBackPressed()
+        if(System.currentTimeMillis() > backPressed + 2500) {
+            backPressed = System.currentTimeMillis()
+            Toast.makeText(applicationContext, "Back 버튼을 한번 더 누르면 종료합니다.",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if(System.currentTimeMillis() <= backPressed + 2500) {
+            finishAffinity()
+        }
+    }
 }
