9dc7c386bfb83a68d02f1f809197dc785a54adcc
ohksj77 <ohksj77@gmail.com>
2023-02-03 13:12:09 +0900

    chore: repair fixed api

--- scrapping/app.py
+++ scrapping/app.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
@@ -39,12 +41,14 @@ class Search(Resource):
         search_type = request.args.get('type')
         page = request.args.get('page')
         
-        driver.get('https://github.com/search?q=' + name + '&type=' + search_type + '&p=' + page)
+        result = requests.get('https://github.com/search?q=' + name + '&type=' + search_type + '&p=' + page)
+        result.raise_for_status()
+        soup = BeautifulSoup(result.text, "lxml")
         
         if search_type == 'repositories':
-            tag_list = self.get_repository()
+            tag_list = self.get_repository(soup)
         elif search_type == 'users':
-            tag_list = self.get_user()
+            tag_list = self.get_user(soup)
             
         for tag in tag_list:
             results.append(tag.text)
@@ -57,12 +61,13 @@ class Search(Resource):
         
         return (response, 200)
     
-    def get_repository(self):
-        repo_list = driver.find_element(By.CLASS_NAME, 'repo-list')
-        return repo_list.find_elements(By.CLASS_NAME, "v-align-middle")
+    def get_repository(self, soup):
+        repo_list = soup.find('ul', attrs={"class" : 'repo-list'})
+        return repo_list.find_all('a', attrs={"class" : "v-align-middle"})
         
-    def get_user(self):
-        return driver.find_elements(By.CLASS_NAME, 'mr-1')
+    def get_user(self, soup):
+        user_list = soup.find_all('a', attrs={"class" : 'mr-1'})
+        return user_list
 
 @ns.route('/scrap/commits', methods=['GET'])
 class MemberCommit(Resource):
@@ -72,20 +77,42 @@ class MemberCommit(Resource):
         member = request.args.get('member')
         year = request.args.get('year')
         
-        driver.get('https://github.com/' + member + '?tab=overview&from=' + year + '-01-01')
+        result = requests.get('https://github.com/' + member + '?tab=overview&from=' + year + '-01-01')
+        result.raise_for_status()
         
-        h2 = driver.find_element(By.CLASS_NAME, 'f4 text-normal mb-2')
-        
-        profile_img = driver.find_element(By.CLASS_NAME, 'avatar avatar-user width-full border color-bg-default')
-        profile_img = profile_img['src']
-        
-        name = driver.find(By.CLASS_NAME, 'p-name vcard-fullname d-block overflow-hidden').text
-        
+        soup = BeautifulSoup(result.text, "lxml")
+        h2 = soup.find('h2', attrs={"class" : "f4 text-normal mb-2"})
         commit_num = int(h2.text.strip().split(' ')[0].rstrip())
         
-        producer.send('gitrank.to.backend.commit', value={"commitNum" : commit_num, "name" : name, "githubId" : member, "profileImage" : profile_img})
+        producer.send('gitrank.to.backend.commit', value={"commitNum" : commit_num, "githubId" : member})
 
         return ('success', 200)
+    
+@ns.route('/scrap/git-repos', methods=['GET'])
+class GitRepos(Resource):
+    
+    '''유저 페이지의 커밋 수를 스크래핑한다'''
+    def get(self):
+        name = request.args.get('name')
+        year = request.args.get('year')
+        
+        driver = webdriver.Remote()
+        
+        driver.get('https://github.com/' + name + '/graphs/contributors?from=2023-01-01&to=' + str(year + 1) + '-12-31&type=c')
+        
+        elements = driver.find_elements(By.CLASS_NAME, 'border-bottom p-2 lh-condensed')
+        
+        response = {}
+        
+        if not elements:
+            return ('No Content', 200)
+        
+        for e in elements:
+            member_name = e.find_element(By.CLASS_NAME, 'text-normal')
+            commits = e.find_element(By.CLASS_NAME, 'Link--secondary text-normal')
+            response[member_name] = int(commits.split(' ')[0])
+        
+        return (response, 200)
         
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
