d1fb823587adc85ea14d3452ced66f15bf9623de
JJ <jeonghojin03@gmail.com>
2023-03-06 22:00:45 +0900

    pref: 레포지토리 상세 조회 MVVM 패턴 코드 수정

--- ios/ios/Services/RepoContributorInfoService.swift
+++ ios/ios/Services/RepoContributorInfoService.swift
@@ -7,33 +7,32 @@
 
 import Foundation
 import Alamofire
+import RxSwift
 
 final class RepoContributorInfoService{
-    static let repoShared = RepoContributorInfoService()
     let ip = APIURL.ip
-    var selectedName = ""
-    var checkData = false
-    var resultData = [RepoContributorInfoModel]()
     
     /// 레포지토리 내부 Contributor 정보
-    func getRepoContriInfo(){
+    func getRepoContriInfo(selectedName: String) -> Observable<[RepoContributorInfoModel]> {
         let url = APIURL.apiUrl.getRepoContributorInfo(ip: ip, name: selectedName)
-        resultData = []
-        
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
-            AF.request(url, method: .get)
-                .validate(statusCode: 200..<201)
-                .responseDecodable(of: [RepoContriInfoDecodingModel].self) { response in
-                    guard let responseResult = response.value else {return}
-                    if(responseResult.count > 0 && self.resultData.count == 0){
-                        self.checkData = true
-                        timer.invalidate()
-                        for data in responseResult{
-                            self.resultData.append(RepoContributorInfoModel(githubId: data.githubId, commits: data.commits, additions: data.additions, deletions: data.deletions))
+        var resultData = [RepoContributorInfoModel]()
+
+        return Observable.create(){ observer in
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+                AF.request(url, method: .get)
+                    .validate(statusCode: 200..<201)
+                    .responseDecodable(of: [RepoContriInfoDecodingModel].self) { response in
+                        guard let responseResult = response.value else {return}
+                        if responseResult.count > 0 && resultData.count == 0 {
+                            timer.invalidate()
+                            for data in responseResult{
+                                resultData.append(RepoContributorInfoModel(githubId: data.githubId, commits: data.commits, additions: data.additions, deletions: data.deletions))
+                                observer.onNext(resultData)
+                            }
                         }
                     }
-                }
-        })
-       
+            })
+            return Disposables.create()
+        }
     }
 }
