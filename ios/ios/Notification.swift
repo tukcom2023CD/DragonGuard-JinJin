4773807beb474003d7eeb62b3a1dceccee26530b
JJ <jeonghojin03@gmail.com>
2023-03-08 00:45:19 +0900

    feat: Language filtering screen complete

--- ios/ios/Notification.swift
+++ ios/ios/Notification.swift
@@ -9,19 +9,6 @@ import Foundation
 
 extension Notification.Name{
     static let data = Notification.Name("")
-    static let deepLink = Notification.Name("")
-    static let walletAddress = Notification.Name("")
-    static let clickedRepo = Notification.Name("")
-}
-
-// KLIP 지갑 주소
-enum NotificationWalletAddress{
-    case walletAddress
-}
-
-// KLIP Deep Link
-enum NotificationDeepLinkKey{
-    case link
 }
 
 // 비교하기 -> 레포지토리 한개 씩 고르는 곳
@@ -29,8 +16,3 @@ enum NotificationKey{
     case choiceId
     case repository
 }
-
-enum NotificationUsers{
-    case repo
-}
-
