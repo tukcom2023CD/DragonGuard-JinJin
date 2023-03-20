dde32ac5bb04ed287bc015570331ed16b892111e
ohksj77 <ohksj77@gmail.com>
2023-03-20 22:47:18 +0900

    feat: delete permitting logic to auth controller

--- backend/src/main/java/com/dragonguard/backend/member/service/AuthService.java
+++ backend/src/main/java/com/dragonguard/backend/member/service/AuthService.java
@@ -4,19 +4,18 @@ import com.dragonguard.backend.config.security.exception.JwtProcessingException;
 import com.dragonguard.backend.config.security.jwt.JwtSetupService;
 import com.dragonguard.backend.config.security.jwt.JwtToken;
 import com.dragonguard.backend.config.security.jwt.JwtTokenProvider;
+import com.dragonguard.backend.config.security.jwt.JwtValidator;
 import com.dragonguard.backend.config.security.oauth.user.UserDetailsImpl;
 import com.dragonguard.backend.global.exception.EntityNotFoundException;
 import com.dragonguard.backend.member.entity.Member;
 import com.dragonguard.backend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -26,6 +25,7 @@ public class AuthService {
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final JwtSetupService jwtSetupService;
+    private final JwtValidator jwtValidator;
 
     @Transactional
     public String refreshToken(String oldRefreshToken, String oldAccessToken, HttpServletResponse response) {
@@ -37,7 +37,7 @@ public class AuthService {
             throw new JwtProcessingException();
         }
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Authentication authentication = jwtValidator.getAuthentication(oldAccessToken);
         UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
 
         UUID id = UUID.fromString(user.getName());
