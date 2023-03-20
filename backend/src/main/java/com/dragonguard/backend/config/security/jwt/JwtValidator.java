e79524398ba7871be93a33f7c67f4e3facbe1872
ohksj77 <ohksj77@gmail.com>
2023-03-20 22:59:48 +0900

    fix: circular reference

--- backend/src/main/java/com/dragonguard/backend/config/security/jwt/JwtValidator.java
+++ backend/src/main/java/com/dragonguard/backend/config/security/jwt/JwtValidator.java
@@ -2,8 +2,9 @@ package com.dragonguard.backend.config.security.jwt;
 
 import com.dragonguard.backend.config.security.oauth.user.UserDetailsImpl;
 import com.dragonguard.backend.config.security.oauth.user.UserDetailsMapper;
+import com.dragonguard.backend.global.exception.EntityNotFoundException;
 import com.dragonguard.backend.member.entity.Member;
-import com.dragonguard.backend.member.service.MemberService;
+import com.dragonguard.backend.member.repository.MemberRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +24,13 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class JwtValidator {
     private final Key key;
-    private final MemberService memberService;
+    private final MemberRepository memberRepository;
     private final UserDetailsMapper userDetailsMapper;
 
     public Authentication getAuthentication(String accessToken) {
         Claims claims = getTokenBodyClaims(accessToken);
-        Member member = memberService.getEntity(extractUUID(claims));
+        Member member = memberRepository.findById(extractUUID(claims))
+                .orElseThrow(EntityNotFoundException::new);
         UserDetailsImpl loginUser = userDetailsMapper.mapToLoginUser(member);
 
         return new UsernamePasswordAuthenticationToken(loginUser, "", loginUser.getAuthorities());
