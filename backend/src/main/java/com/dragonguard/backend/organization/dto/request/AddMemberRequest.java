71c26efe61ea87ea7df7eca8f5922617c2977c0a
ohksj77 <ohksj77@gmail.com>
2023-03-21 17:22:09 +0900

    chore: add missing constraints

--- backend/src/main/java/com/dragonguard/backend/organization/dto/request/AddMemberRequest.java
+++ backend/src/main/java/com/dragonguard/backend/organization/dto/request/AddMemberRequest.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddMemberRequest {
+    @NotNull
     private Long organizationId;
 }
