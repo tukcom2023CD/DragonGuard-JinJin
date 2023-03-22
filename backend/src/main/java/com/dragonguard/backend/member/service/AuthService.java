293933618403895932bfe2e56bd925291b829151
ohksj77 <ohksj77@gmail.com>
2023-03-22 12:35:30 +0900

    feat: delete cookie logic and fix main logic in refresh api

--- backend/src/main/java/com/dragonguard/backend/member/service/AuthService.java
+++ backend/src/main/java/com/dragonguard/backend/member/service/AuthService.java
@@ -1,16 +1,12 @@
 package com.dragonguard.backend.member.service;
 
-import com.dragonguard.backend.config.security.exception.CookieException;
 import com.dragonguard.backend.config.security.exception.JwtProcessingException;
-import com.dragonguard.backend.config.security.jwt.JwtSetupService;
 import com.dragonguard.backend.config.security.jwt.JwtToken;
 import com.dragonguard.backend.config.security.jwt.JwtTokenProvider;
 import com.dragonguard.backend.config.security.jwt.JwtValidator;
 import com.dragonguard.backend.config.security.oauth.user.UserDetailsImpl;
-import com.dragonguard.backend.global.exception.EntityNotFoundException;
 import com.dragonguard.backend.member.entity.Member;
 import com.dragonguard.backend.member.repository.MemberRepository;
-import com.dragonguard.backend.util.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,27 +14,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
+
+/**
+ * @author 김승진
+ * @description 멤버 인증 관련 서비스 로직을 담당하는 클래스
+ */
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
-    private final JwtSetupService jwtSetupService;
     private final JwtValidator jwtValidator;
-    private final CookieUtil cookieUtil;
 
     @Transactional
-    public String refreshToken(String oldRefreshToken, String oldAccessToken, HttpServletRequest request, HttpServletResponse response) {
+    public JwtToken refreshToken(String oldRefreshToken, String oldAccessToken) {
         if (!StringUtils.hasText(oldRefreshToken) || !StringUtils.hasText(oldAccessToken)) {
             throw new IllegalArgumentException();
         }
 
-        if (!jwtTokenProvider.validateToken(oldAccessToken)) {
+        if (!jwtTokenProvider.validateToken(oldRefreshToken)) {
             throw new JwtProcessingException();
         }
 
@@ -53,15 +49,12 @@ public class AuthService {
             throw new JwtProcessingException();
         }
 
-        cookieUtil.deleteCookie(request, response, "Access");
-        cookieUtil.deleteCookie(request, response, "Refresh");
+        JwtToken jwtToken = jwtTokenProvider.createToken(user);
 
-        JwtToken jwtToken = jwtSetupService.addJwtTokensInCookie(response, user);
-
-        Member member = memberRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        Member member = user.getMember();
         member.updateRefreshToken(jwtToken.getRefreshToken());
 
-        return jwtToken.getAccessToken();
+        return jwtToken;
     }
 
     public Member getLoginUser() {
