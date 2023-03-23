9f6c8b06c3d6d4ab03e6a73da50e60c067c46ee8
JJ <jeonghojin03@gmail.com>
2023-03-23 16:19:50 +0900

    chore: 타입 선택 화면 수정

--- ios/ios/View/SettingScreen/CertifiedOrganization/ChooseOrganizationType.swift
+++ ios/ios/View/SettingScreen/CertifiedOrganization/ChooseOrganizationType.swift
@@ -15,21 +15,24 @@ final class ChooseOrganizationType: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addToView()
     }
     
     lazy var typeTableView: UITableView = {
         let tableview = UITableView()
         
         tableview.register(ChooseOrganizationTypeCell.self, forCellReuseIdentifier: ChooseOrganizationTypeCell.identifier)
-        view.addSubview(tableview)
+        return tableview
+    }()
+    
+    private func addToView(){
+        self.view.addSubview(self.typeTableView)
         
-        tableview.snp.makeConstraints({ make in
+        self.typeTableView.snp.makeConstraints({ make in
             make.top.bottom.equalTo(view.safeAreaLayoutGuide)
             make.leading.trailing.equalTo(0)
         })
-        
-        return tableview
-    }()
+    }
     
 }
 
@@ -37,7 +40,9 @@ final class ChooseOrganizationType: UIViewController{
 extension ChooseOrganizationType: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ChooseOrganizationTypeCell.identifier, for: indexPath) as! ChooseOrganizationTypeCell
-        
+        cell.inputText(text: oraganizatinoList[indexPath.row])
+        cell.backgroundColor = UIColor(red: 255/255, green: 194/255, blue: 194/255, alpha: 0.5) /* #ffc2c2 */
+        cell.layer.cornerRadius = 20
         return cell
     }
     
@@ -55,4 +60,21 @@ extension ChooseOrganizationType: UITableViewDelegate, UITableViewDataSource{
 
 protocol SendType{
     func sendType(type: String)
+}
+
+
+
+import SwiftUI
+
+struct VCPreViewChooseOrganizationType:PreviewProvider {
+    static var previews: some View {
+        ChooseOrganizationType().toPreview().previewDevice("iPhone 14 Pro")
+        // 실행할 ViewController이름 구분해서 잘 지정하기
+    }
+}
+struct VCPreViewChooseOrganizationType2:PreviewProvider {
+    static var previews: some View {
+        ChooseOrganizationType().toPreview().previewDevice("iPad (10th generation)")
+        // 실행할 ViewController이름 구분해서 잘 지정하기
+    }
 }
