7754fa53193816ca2eeeec38508a0154dff457ff
JJ <jeonghojin03@gmail.com>
2023-04-02 18:47:49 +0900

    feat: 조직 인증 시 사용자가 모든 정보 입력했는지 확인

---
+++ ios/ios/View/SettingScreen/Certification/CertificationEmail/CertificationEmailController.swift
@@ -0,0 +1,23 @@
+//
+//  CertificationEmailController.swift
+//  ios
+//
+//  Created by 정호진 on 2023/04/02.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import RxSwift
+
+final class CertificationEmailController: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+    
+    
+    
+}
