682e98b1eee67feec03862f8ea210956f06cce4f
posite <su84053@gmail.com>
2023-04-03 22:37:41 +0900

    feat: 조직 내 사용자의 랭킹 화면 구성

--- android/app/src/main/java/com/dragonguard/android/activity/ranking/MyOrganizationInternalActivity.kt
+++ android/app/src/main/java/com/dragonguard/android/activity/ranking/MyOrganizationInternalActivity.kt
@@ -8,18 +8,23 @@ import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import com.dragonguard.android.R
 import com.dragonguard.android.activity.MainActivity
-import com.dragonguard.android.databinding.ActivityMyUniversityInternalRankingBinding
+import com.dragonguard.android.databinding.ActivityMyOrganizationInternalRankingBinding
+import com.dragonguard.android.recycleradapter.TotalUsersRankingAdapter
+import com.dragonguard.android.viewmodel.Viewmodel
 
 /*
  사용자의 대학교 내의 랭킹을 보여주는 activity
  */
-class MyUniversityInternalActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMyUniversityInternalRankingBinding
+class MyOrganizationInternalActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMyOrganizationInternalRankingBinding
+    private var viewmodel = Viewmodel()
     private var token = ""
+    private var orgName = ""
+    private lateinit var organizationInternalRankingAdapter: TotalUsersRankingAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_my_university_internal_ranking)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_my_organization_internal_ranking)
+        binding.organizationInternalViewmodel = viewmodel
 
         setSupportActionBar(binding.toolbar) //커스텀한 toolbar를 액션바로 사용
         supportActionBar?.setDisplayShowTitleEnabled(false)
@@ -27,6 +32,8 @@ class MyUniversityInternalActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
 
         token = intent.getStringExtra("token")!!
+        orgName = intent.getStringExtra("organization")!!
+
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.home, binding.toolbar.menu)
