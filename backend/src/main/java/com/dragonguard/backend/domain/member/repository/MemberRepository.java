9c06034b2f8e3f75abca4dc6c2c8926bf765349a
ohksj77 <ohksj77@gmail.com>
2023-06-02 15:58:10 +0900

    fix: dev profile execute error

--- backend/src/main/java/com/dragonguard/backend/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/dragonguard/backend/domain/member/repository/MemberRepository.java
@@ -2,22 +2,17 @@ package com.dragonguard.backend.domain.member.repository;
 
 import com.dragonguard.backend.domain.member.entity.Member;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface MemberRepository {
     Optional<Member> findMemberByGithubId(String githubId);
 
-    boolean existsByGithubId(String githubId);
-
     String findRefreshTokenById(UUID id);
 
     void updateRefreshToken(UUID id, String token);
 
-    List<Member> findAll();
-
     Optional<Member> findById(UUID id);
 
-    Member save(Member member);
+    <S extends Member> S save(S entity);
 }
