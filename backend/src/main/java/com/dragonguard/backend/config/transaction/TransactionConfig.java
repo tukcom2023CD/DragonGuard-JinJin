d5f3dec62a33efc4e5647a68ac7d9d9d6cbac079
ohksj77 <ohksj77@gmail.com>
2023-06-21 20:19:23 +0900

    chore: fix aspectj implementation

--- backend/src/main/java/com/dragonguard/backend/config/transaction/TransactionConfig.java
+++ backend/src/main/java/com/dragonguard/backend/config/transaction/TransactionConfig.java
@@ -2,12 +2,13 @@ package com.dragonguard.backend.config.transaction;
 
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableAspectJAutoProxy
 @EnableTransactionManagement(proxyTargetClass = true, mode = AdviceMode.ASPECTJ)
 @EnableLoadTimeWeaving(aspectjWeaving = EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
 public class TransactionConfig {
 }
-
