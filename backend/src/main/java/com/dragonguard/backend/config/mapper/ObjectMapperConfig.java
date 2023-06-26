5a6c0664a300a0145f78ee57390234e71a359d49
김승진 <ohksj77@gmail.com>
2023-06-26 15:56:23 +0900

    chore: add naming strategy to objectmapper bean

--- backend/src/main/java/com/dragonguard/backend/config/mapper/ObjectMapperConfig.java
+++ backend/src/main/java/com/dragonguard/backend/config/mapper/ObjectMapperConfig.java
@@ -2,8 +2,11 @@ package com.dragonguard.backend.config.mapper;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * @author 김승진
@@ -13,9 +16,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ObjectMapperConfig {
     @Bean
+    @Primary
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+                .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
     }
 }
