11337646a9e170e07901b9717f06f5a121505cc0
김승진 <ohksj77@gmail.com>
2023-07-03 22:51:49 +0900

    chore: change lock mode

--- backend/src/main/java/com/dragonguard/backend/domain/blockchain/repository/JpaBlockchainRepository.java
+++ backend/src/main/java/com/dragonguard/backend/domain/blockchain/repository/JpaBlockchainRepository.java
@@ -1,11 +1,23 @@
 package com.dragonguard.backend.domain.blockchain.repository;
 
 import com.dragonguard.backend.domain.blockchain.entity.Blockchain;
+import com.dragonguard.backend.domain.blockchain.entity.ContributeType;
+import com.dragonguard.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
+
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+import java.util.List;
 
 /**
  * @author 김승진
  * @description 블록체인 관련 DB와의 CRUD를 담당하는 클래스
  */
 
-public interface JpaBlockchainRepository extends JpaRepository<Blockchain, Long>, BlockchainRepository {}
+public interface JpaBlockchainRepository extends JpaRepository<Blockchain, Long>, BlockchainRepository {
+    @Lock(LockModeType.OPTIMISTIC)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value ="1000")})
+    List<Blockchain> findAllByMemberAndContributeType(Member member, ContributeType contributeType);
+}
