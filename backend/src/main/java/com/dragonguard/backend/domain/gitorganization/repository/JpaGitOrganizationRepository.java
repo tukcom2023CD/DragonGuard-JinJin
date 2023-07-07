328147fb4fb14ee808879f8b91d9b61bbf50951e
김승진 <ohksj77@gmail.com>
2023-07-07 20:04:36 +0900

    feat: add db lock to git org

--- backend/src/main/java/com/dragonguard/backend/domain/gitorganization/repository/JpaGitOrganizationRepository.java
+++ backend/src/main/java/com/dragonguard/backend/domain/gitorganization/repository/JpaGitOrganizationRepository.java
@@ -3,8 +3,12 @@ package com.dragonguard.backend.domain.gitorganization.repository;
 import com.dragonguard.backend.domain.gitorganization.entity.GitOrganization;
 import com.dragonguard.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.util.List;
 
 /**
@@ -16,4 +20,8 @@ public interface JpaGitOrganizationRepository extends JpaRepository<GitOrganizat
 
     @Query("SELECT DISTINCT go FROM GitOrganization go JOIN FETCH go.gitOrganizationMembers gom JOIN FETCH gom.member m WHERE m = :member")
     List<GitOrganization> findAllByMember(Member member);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value ="1500")})
+    boolean existsByName(String name);
 }
