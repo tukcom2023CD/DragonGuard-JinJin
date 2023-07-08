cd6b196e6556a831702d6ceab0106ec844221aac
posite <su84053@gmail.com>
2023-07-08 22:04:19 +0900

    feat: 다른 레포의 같은 사람 활동 비교 가능하게 변경

--- android/app/src/main/java/com/dragonguard/android/fragment/UserSheetfragment.kt
+++ android/app/src/main/java/com/dragonguard/android/fragment/UserSheetfragment.kt
@@ -10,34 +10,33 @@ import com.dragonguard.android.activity.search.SearchFilterActivity
 import com.dragonguard.android.adapters.UserListAdapter
 import com.dragonguard.android.databinding.FilterSheetBinding
 import com.dragonguard.android.databinding.FragmentCompareUserBinding
+import com.dragonguard.android.databinding.UserSheetBinding
 import com.dragonguard.android.model.contributors.GitRepoMember
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class UserSheetfragment (private val context: CompareUserFragment, private val list: ArrayList<GitRepoMember>, private val type: Int
-                         , private val fragmentBinding: FragmentCompareUserBinding
+class UserSheetfragment (private val context: CompareUserFragment, private val firstList: ArrayList<GitRepoMember>,
+                         private val second: ArrayList<GitRepoMember>, private val type: Int, private val repo1: String, private val repo2: String,
+                         private val fragmentBinding: FragmentCompareUserBinding
 ) : BottomSheetDialogFragment() {
-    private lateinit var binding: FilterSheetBinding
+    private lateinit var binding: UserSheetBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FilterSheetBinding.inflate(inflater, container, false)
+        binding = UserSheetBinding.inflate(inflater, container, false)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        when(type) {
-            1 -> {
-                binding.filterTitle.text = "User1"
-            }
-            2 -> {
-                binding.filterTitle.text = "User2"
-            }
-        }
-        binding.filterItems.adapter = UserListAdapter(list, context, type, fragmentBinding)
-        binding.filterItems.layoutManager = LinearLayoutManager(requireContext())
-        binding.filterItems.adapter?.notifyDataSetChanged()
+        binding.filterTitleFirst.text = repo1
+        binding.filterTitleSecond.text = repo2
+        binding.firstFilterItems.adapter = UserListAdapter(firstList, context, type, fragmentBinding)
+        binding.firstFilterItems.layoutManager = LinearLayoutManager(requireContext())
+        binding.firstFilterItems.adapter?.notifyDataSetChanged()
 
+        binding.secondFilterItems.adapter = UserListAdapter(second, context, type, fragmentBinding)
+        binding.secondFilterItems.layoutManager = LinearLayoutManager(requireContext())
+        binding.secondFilterItems.adapter?.notifyDataSetChanged()
     }
 }
