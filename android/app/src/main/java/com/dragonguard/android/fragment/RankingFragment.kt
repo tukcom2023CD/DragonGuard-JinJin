db8369517aad0960d53ceb1dcaabfce9b18e7c2c
posite <su84053@gmail.com>
2023-07-09 23:14:22 +0900

    feat: 안드로이드 랭킹 tab의 ui 오류 수정

--- android/app/src/main/java/com/dragonguard/android/fragment/RankingFragment.kt
+++ android/app/src/main/java/com/dragonguard/android/fragment/RankingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.dragonguard.android.R
 import com.dragonguard.android.adapters.FragmentAdapter
 import com.dragonguard.android.databinding.FragmentRankingBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -27,7 +29,10 @@ class RankingFragment(private val token: String) : Fragment() {
         binding.fragmentContent.adapter = FragmentAdapter(this, token = token)
         val tabs = arrayOf("사용자 전체", "조직 전체" ,"회사", "대학교", "고등학교", "ETC")
         TabLayoutMediator(binding.rankingTab, binding.fragmentContent) { tab, position ->
-            tab.text = tabs[position]
+            val tabView = LayoutInflater.from(requireContext()).inflate(R.layout.custom_text, null) as TextView
+            tabView.text = tabs[position]
+            tab.customView = tabView
+//            tab.text = tabs[position]
         }.attach()
     }
 
