8736334a8b621fdeb735548e4da5d06026746d19
김승진 <ohksj77@gmail.com>
2023-07-27 20:28:52 +0900

    fix: batch writer

--- backend/src/main/java/com/dragonguard/backend/batch/GitRepoMemberWriter.java
+++ backend/src/main/java/com/dragonguard/backend/batch/GitRepoMemberWriter.java
@@ -2,6 +2,7 @@ package com.dragonguard.backend.batch;
 
 import com.dragonguard.backend.domain.gitrepomember.entity.GitRepoMember;
 import com.dragonguard.backend.domain.gitrepomember.repository.GitRepoMemberRepository;
+import com.dragonguard.backend.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
@@ -14,12 +15,14 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class GitRepoMemberWriter implements ItemWriter<List<GitRepoMember>> {
     private final GitRepoMemberRepository gitRepoMemberRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     @Transactional
     public void write(final List<? extends List<GitRepoMember>> items) throws Exception {
         List<GitRepoMember> result = items.stream()
                 .flatMap(List::stream)
+                .peek(i -> memberRepository.findByGithubId(i.getMember().getGithubId()).orElseGet(() -> memberRepository.save(i.getMember())))
                 .collect(Collectors.toList());
 
         gitRepoMemberRepository.saveAll(result);
