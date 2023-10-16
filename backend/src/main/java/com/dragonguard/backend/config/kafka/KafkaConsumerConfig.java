1367694d84ac53c2479a5fd84aee2773c813b32d
김승진 <ohksj77@gmail.com>
2023-10-16 22:54:38 +0900

    fix: apply json deserializer to kafka consumer

--- backend/src/main/java/com/dragonguard/backend/config/kafka/KafkaConsumerConfig.java
+++ backend/src/main/java/com/dragonguard/backend/config/kafka/KafkaConsumerConfig.java
@@ -51,7 +51,7 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, String.class);
 
         return new DefaultKafkaConsumerFactory<>(props);
