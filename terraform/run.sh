c5648815e1a11ade01331dc005482976ef3e4510
김승진 <ohksj77@gmail.com>
2024-03-18 03:47:40 +0900

    chore: remove terraform script

--- terraform/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Docker (docker-desktop 등) 실행 상태에서 실행해주세요. 
-# init, plan, apply, destroy 등을 인자로 넘겨주세요.
-# 예시: sh run.sh init
-
-docker run -i -t -v $(pwd):/app -w /app hashicorp/terraform:latest $1
